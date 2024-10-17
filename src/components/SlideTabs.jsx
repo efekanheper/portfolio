d715d8681e6262ffc59076588473046a90ee1470
Efekan Heper <148638730+efekanheper@users.noreply.github.com>
2024-10-18 02:34:56 +0300

    Update SlideTabs.jsx
    stil değişikliği

--- src/components/SlideTabs.jsx
+++ src/components/SlideTabs.jsx
@@ -20,7 +20,8 @@ const SlideTabs = () => {
           opacity: 0,
         }));
       }}
-      className="relative top-4 mx-auto flex w-fit rounded-full border-2 border-black bg-neutral-700 p-1"
+      className="fixed top-0 left-0 right-0 mt-5 z-50 mx-auto flex w-fit  rounded-3xl border border-neutral-800 bg-black/20 backdrop-blur-md p-1"
+      style={{ transition: "background-color 0.3s ease" }}
     >
       <Tab setPosition={setPosition} onClick={() => handleClick("home")}>
         About
@@ -54,7 +55,7 @@ const Tab = ({ children, setPosition, onClick }) => {
         });
       }}
       onClick={onClick}
-      className="uppercase  relative z-10 block cursor-pointer px-3 py-1.5 text-xs text-white mix-blend-difference md:px-3 md:py-2 md:text-sm"
+      className="uppercase relative z-10 block cursor-pointer px-3 py-1.5 text-xs text-white hover:text-black  md:px-3 md:py-2 md:text-sm"
     >
       {children}
     </li>
